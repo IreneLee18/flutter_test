977f80031d0f1973f0bfb1ac13219a9d5e898753
IreneLee <irene@jgallop.com>
2024-01-12 16:41:00 +0800

    20240112 --> 試著召喚相機並且拍照片（IOS無法用開發者測試）

--- lib/favorite_places/widgets/image_input.dart
+++ lib/favorite_places/widgets/image_input.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
+import 'package:date_format/date_format.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ImageInput extends StatefulWidget {
   const ImageInput({super.key});
@@ -10,7 +14,23 @@ class ImageInput extends StatefulWidget {
 }
 
 class _ImageInputState extends State<ImageInput> {
-  void _takePicture() {}
+  File? _selectedImage;
+
+  void _takePicture() async {
+    final imagePicker = ImagePicker();
+    // 選擇你要取得照片的來源
+    // ImageSource.camera：相機
+    // ImageSource.gallery：照片集
+    final pickedImage =
+        await imagePicker.pickImage(source: ImageSource.camera, maxWidth: 600);
+
+    if (pickedImage == null) return;
+
+    // 轉換照片
+    setState(() {
+      _selectedImage = File(pickedImage.path);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,11 +43,23 @@ class _ImageInputState extends State<ImageInput> {
       height: 250,
       width: double.infinity,
       alignment: Alignment.center,
-      child: TextButton.icon(
-        icon: const Icon(Icons.camera),
-        label: const Text('Take Picture'),
-        onPressed: _takePicture,
-      ),
+      child: _selectedImage != null
+          ? GestureDetector(
+            // GestureDetector：使我這個 widget 可以使用任何我想要用的 event
+            // 這裡選擇的是：點擊（onTap）要在叫出相機
+            onTap: _takePicture,
+            child: Image.file(
+                _selectedImage!,
+                fit: BoxFit.cover,
+                width: double.infinity,
+                height: double.infinity,
+              ),
+          )
+          : TextButton.icon(
+              icon: const Icon(Icons.camera),
+              label: const Text('Take Picture'),
+              onPressed: _takePicture,
+            ),
     );
   }
 }
