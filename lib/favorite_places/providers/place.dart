71afdf615c20f8547d58c894a09ce20b2a44e969
IreneLee <irene@jgallop.com>
2024-01-14 16:44:24 +0800

    20240114 --> 將資料存取在資料庫中

--- lib/favorite_places/providers/place.dart
+++ lib/favorite_places/providers/place.dart
@@ -4,6 +4,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:test/favorite_places/models/place.dart';
 import 'package:path_provider/path_provider.dart' as syspaths;
 import 'package:path/path.dart' as path;
+import 'package:sqflite/sqflite.dart' as sql;
+import 'package:sqflite/sqlite_api.dart';
 
 class PlaceNotifier extends StateNotifier<List<PlaceItem>> {
   PlaceNotifier() : super(const []);
@@ -27,6 +29,31 @@ class PlaceNotifier extends StateNotifier<List<PlaceItem>> {
       image: copiedImage,
       location: location,
     );
+
+    // 目的：將資料存取在資料庫中
+    // 創建一個資料庫的目錄
+    final dbPath = await sql.getDatabasesPath();
+    // 打開資料庫，並傳入自己創建的資料庫目錄路徑
+    // 當試圖打開一個還不存在的資料庫，sqlLite 會自動為我們創建一個資料庫
+    final db = await sql.openDatabase(
+      path.join(dbPath, 'place.db'),
+      // onCreate: 會在我們第一次創建資料庫時執行
+      onCreate: (db, version) {
+        return db.execute(
+            'CREATE TABLE place(id TEXT PRIMARY, name TEXT, image TEXT, lat REAL, lng REAL, address TEXT)');
+      },
+      version: 1,
+    );
+
+    // 將資料存在資料庫裡面
+    db.insert('place', {
+      'id': newPlace.id,
+      'name': newPlace.name,
+      'image': newPlace.image!.path,
+      'lat': newPlace.location!.latitude,
+      'lng': newPlace.location!.longitude,
+      'address': newPlace.location!.address,
+    });
 
     state = [newPlace, ...state];
   }
